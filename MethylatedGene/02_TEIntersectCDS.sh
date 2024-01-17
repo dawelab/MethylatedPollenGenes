8a10d6747a1fbaa44f1d301c4677c05f4ff8eb0e
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-17 14:25:46 -0500

    Update 02_TEIntersectCDS.sh

--- MethylatedGene/02_TEIntersectCDS.sh
+++ MethylatedGene/02_TEIntersectCDS.sh
@@ -1,1 +1,7 @@
-
+cd ${working_dir}
+ml BEDTools
+#Make the gene bed file for CDS
+gene_cds=
+#Make the TE bed file
+TE=
+bedtools intersect -wa -wb -a ${gene_cds} -b ${TE} | cut -f | uniq > gene_interectTE.bed
