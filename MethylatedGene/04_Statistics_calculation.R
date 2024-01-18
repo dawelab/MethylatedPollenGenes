a881aa8fc824f23b2dc8bcc613786f5f98b5aee6
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-18 13:24:46 -0500

    Update 04_Statistics_calculation.R

--- MethylatedGene/04_Statistics_calculation.R
+++ MethylatedGene/04_Statistics_calculation.R
@@ -1,3 +1,3 @@
 #Merge the redefined epiallele matrix, TE(CDS), DEG dummy variable together.
 df <- merge(B73_all,df_TE, by = "gene", all.x = T)
-df <- megre(df, df_DEG, by = "gene", all.x = T)
+df <- merge(df, df_DEG, by = "gene", all.x = T)
