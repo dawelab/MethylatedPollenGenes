256c41b041c587b09b08c24e6d8097f00b78febd
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-17 22:03:09 -0500

    Update 03_TE_CDS_dummy.R

--- MethylatedGene/03_TE_CDS_dummy.R
+++ MethylatedGene/03_TE_CDS_dummy.R
@@ -1,5 +1,10 @@
-geneTE_list <- read.table()
-
+setwd()
+geneTE_list <- read.table("gene.intersect")
+geneID_TEinter <- geneID_TEinter$V4 %>% unique() 
+names(geneID_TEinter) <- "gene"
+TE_free_gene <- setdiff(all_geneset,geneID_TEinter) 
+df_TE <- data.frame(gene = geneID_TEinter,
+                    TE = "Y")
 all_gene_set <- 
 
 setdiff <- 
