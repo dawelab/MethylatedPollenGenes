cede2624a4b3136e3d40133ae1d69097e3c0c66c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-17 21:55:32 -0500

    Update 02_TEIntersectCDS.sh

--- MethylatedGene/02_TEIntersectCDS.sh
+++ MethylatedGene/02_TEIntersectCDS.sh
@@ -1,7 +1,11 @@
 cd ${working_dir}
+$output_dir
 ml BEDTools
 #Make the gene bed file for CDS
-gene_cds=
+#The gene annotation file is downloaded from maizegdb
+awk '$3=="CDS"' Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3 | cut -f1 -d ';' | sed 's/ID=//g' | sed 's/_P...//g' | awk '{print $1,$4,$5,$9}' OFS="\t" | sort -b -k1,1 -k2,2n -k3,3n > Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gene.bed
+gene_cds=${working_dir}/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gene.bed
 #Make the TE bed file
-TE=
+awk '{print $1,$4,$5,$3}' OFS="\t" Zm-B73-REFERENCE-NAM-5.0.TE.gff3 | grep -v "#" > Zm-B73-REFERENCE-NAM-5.0.TE.bed
+TE=${working_dir}
 bedtools intersect -wa -wb -a ${gene_cds} -b ${TE} | cut -f | uniq > gene_interectTE.bed
