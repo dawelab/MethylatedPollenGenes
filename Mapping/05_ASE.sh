ecd785006939dc0750c3a5f6ed8ad9fb4ce0dca5
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-01-26 09:32:46 -0500

    Create 05_ASE.sh

---
+++ Mapping/05_ASE.sh
@@ -0,0 +1,13 @@
+
+
+cd /scratch/yz77862/Allim/output
+ml Allim
+Allim.py --option-file  
+
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --option-file INPUT_OPTION_FILE
+                        Give Input and parameters AllimOptions file to run
+                        Allim pipeline
+
