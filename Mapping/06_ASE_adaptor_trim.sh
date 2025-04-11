04ee73f47c97a2a380c08ab40ef334e5ed95247f
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-04-10 22:47:06 -0400

    Create 06_ASE_adaptor_trim.sh

---
+++ Mapping/06_ASE_adaptor_trim.sh
@@ -0,0 +1,56 @@
+
+
+list=/scratch/yz77862/Allim/B73v5_Ki11/list
+while read INPUT; do
+
+OUT=/scratch/yz77862/Allim/gene_guide/shell/${INPUT}_com.sh
+    echo '#!/bin/bash'  >> ${OUT} 
+    echo "#SBATCH --job-name=${INPUT}_mapping"   >> ${OUT}            
+    echo "#SBATCH --partition=batch"   >> ${OUT} 
+    echo "#SBATCH --nodes=1"   >> ${OUT}                  
+    echo "#SBATCH --ntasks=1"   >> ${OUT}              
+    echo "#SBATCH --cpus-per-task=18"   >> ${OUT}          
+    echo "#SBATCH --mem=80G"   >> ${OUT}                  
+    echo "#SBATCH --time=010:00:00"   >> ${OUT}             
+    echo "#SBATCH --output=${INPUT}_com_bam.out"   >> ${OUT}         
+    echo "#SBATCH --error=${INPUT}_com_bam.err"   >> ${OUT}         
+    echo " "  >> ${OUT}  
+    echo "ml STAR/2.7.10b-GCC-11.3.0" >> ${OUT}  
+    echo "cd /scratch/yz77862/Allim/gene_guide/round1" >> ${OUT}
+    echo " "  >> ${OUT}
+    echo "thread=18"  >> ${OUT}  
+    echo "index=/scratch/yz77862/Allim/reference/B73v5_Ki11/STAR"  >> ${OUT}  
+    echo "read1=/scratch/yz77862/Allim/B73v5_Ki11/${INPUT}_1.fastq.gz"  >> ${OUT}  
+    echo "read2=/scratch/yz77862/Allim/B73v5_Ki11/${INPUT}_2.fastq.gz"  >> ${OUT}  
+    echo " "  >> ${OUT}  
+    echo "STAR \\"  >> ${OUT}    
+    echo "--runMode alignReads \\"  >> ${OUT}  
+    echo "--genomeDir \${index}  \\"  >> ${OUT}  
+    echo "--twopassMode Basic  \\"  >> ${OUT}  
+    echo "​--runThreadN \$thread \\"  >> ${OUT}  
+    echo "--readFilesIn \${read1} \${read2}\\"  >> ${OUT}  
+    echo "--outSAMtype None \\"  >> ${OUT}  
+    echo "--outFileNamePrefix ${INPUT} \\"  >> ${OUT}  
+    echo "--outFilterScoreMin 50 \\" >> ${OUT}  
+    echo "--outFilterMultimapNmax 10000" >> ${OUT}  
+    echo " "  >> ${OUT}
+    echo "cd /scratch/yz77862/Allim/gene_guide/round2"  >> ${OUT}
+    echo " " >> ${OUT}
+    echo "SJ=/scratch/yz77862/Allim/gene_guide/round1/${INPUT}_STARpass1/SJ.out.tab"  >> ${OUT}
+    echo "STAR \\"  >> ${OUT}
+    echo "--genomeDir \${index} \\"  >> ${OUT}
+    echo "--runThreadN \${thread} \\"  >> ${OUT}
+    echo "--sjdbFileChrStartEnd \${SJ} \\"  >> ${OUT}
+    echo "--runMode alignReads \\"  >> ${OUT}
+    echo "--readFilesIn \${read1} \${read2} \\"  >> ${OUT}
+    echo "--outSAMattributes All \\"  >> ${OUT}
+    echo "--outSAMmapqUnique 10 \\"  >> ${OUT}
+    echo "--outFilterMismatchNmax 3 \\"  >> ${OUT}
+    echo "--outFileNamePrefix ${INPUT}_round-2 \\"  >> ${OUT}
+    echo "--outBAMsortingThreadN 4 \\"  >> ${OUT}
+    echo "--outSAMtype BAM SortedByCoordinate \\"  >> ${OUT}
+    echo "--outFilterScoreMin 50 \\" >> ${OUT}  
+    echo "--outFilterMultimapNmax 10000 \\" >> ${OUT}  
+    echo "--outWigType bedGraph read1_5p"  >> ${OUT}
+    sbatch ${OUT}
+done < <(cut -f1,2 ${list} | grep -v 'skip' | sort -u)
