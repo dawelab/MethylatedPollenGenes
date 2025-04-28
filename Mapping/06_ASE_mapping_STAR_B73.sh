0ea53b924ba4c32555c2c2442dc8abb2ae01b3e7
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-04-28 14:48:53 -0400

    Update 06_ASE_mapping_STAR_B73.sh

--- Mapping/06_ASE_mapping_STAR_B73.sh
+++ Mapping/06_ASE_mapping_STAR_B73.sh
@@ -21,37 +21,12 @@ OUT=/scratch/yz77862/Allim/gene_guide/shell/${INPUT}_B73.sh
     echo "read1=/scratch/yz77862/Allim/B73v5_Ki11/trim/${INPUT}_1_val_1.fq.gz"  >> ${OUT}  
     echo "read2=/scratch/yz77862/Allim/B73v5_Ki11/trim/${INPUT}_2_val_2.fq.gz"  >> ${OUT}  
     echo " "  >> ${OUT}  
-    echo "STAR \\"  >> "${OUT}"
-    echo "--runMode alignReads \\"  >> "${OUT}"
-    echo "--genomeDir \${index} \\"  >> "${OUT}"
-    echo "--twopassMode Basic \\"  >> "${OUT}"
-    echo "--runThreadN \${thread} \\"  >> "${OUT}"
-    echo "--readFilesCommand zcat \\"  >> "${OUT}"
-    echo "--readFilesIn \${read1} \${read2} \\"  >> "${OUT}"
-    echo "--outSAMtype None \\"  >> "${OUT}"   # If you want no output SAM, otherwise set to BAM Unsorted
-    echo "--outFileNamePrefix ${INPUT} \\"  >> "${OUT}"
-    echo "--outFilterScoreMin 50 \\" >> "${OUT}"
-    echo "--outFilterMultimapNmax 10000" >> "${OUT}"
-    echo " "  >> ${OUT}
-    echo "cd /scratch/yz77862/Allim/gene_guide/B73/round2"  >> ${OUT}
-    
-    echo " " >> ${OUT}
- # 2nd STAR run
-    echo "SJ=/scratch/yz77862/Allim/gene_guide/B73/round1/${INPUT}_STARpass1/SJ.out.tab"  >> "${OUT}"
-    echo "STAR \\"  >> "${OUT}"
-    echo "--genomeDir \${index} \\"  >> "${OUT}"
-    echo "--runThreadN \${thread} \\"  >> "${OUT}"
-    echo "--readFilesCommand zcat \\"  >> "${OUT}"
-    echo "--sjdbFileChrStartEnd \${SJ} \\"  >> "${OUT}"
-    echo "--runMode alignReads \\"  >> "${OUT}"
-    echo "--readFilesIn \${read1} \${read2} \\"  >> "${OUT}"
-    echo "--outSAMattributes All \\"  >> "${OUT}"
-    echo "--outSAMmapqUnique 10 \\"  >> "${OUT}"
-    echo "--outFilterMismatchNmax 3 \\"  >> "${OUT}"
-    echo "--outFileNamePrefix ${INPUT}_round-2 \\"  >> "${OUT}"
-    echo "--outBAMsortingThreadN 4 \\"  >> "${OUT}"
-    echo "--outSAMtype BAM SortedByCoordinate \\"  >> "${OUT}"
-    echo "--outFilterScoreMin 50 \\" >> "${OUT}"
-    echo "--outFilterMultimapNmax 10000 \\" >> "${OUT}"
-    echo "--outWigType bedGraph read1_5p"  >> "${OUT}"
+    echo "STAR --genomeDir \${index} \\" >> "${OUT}"
+    echo "--runThreadN 6 \\" >> "${OUT}"
+    echo "--readFilesIn \${read1} \${read2} \\" >> "${OUT}"
+    echo "--readFilesCommand zcat \\" >> "${OUT}"
+    echo "--outSAMtype BAM SortedByCoordinate \\" >> "${OUT}"
+    echo "--outSAMunmapped Within \\" >> "${OUT}"
+    echo "--outSAMattributes Standard \\" >> "${OUT}"
+    echo "--outFileNamePrefix \${INPUT}" >> "${OUT}"
 done < <(cut -f1,2 ${list} | grep -v 'skip' | sort -u)
