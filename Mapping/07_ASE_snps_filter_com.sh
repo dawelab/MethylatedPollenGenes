0ace8a497108d8967b3d20cab5670f00ede6903f
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-04-28 14:41:23 -0400

    Update 07_ASE_snps_filter_com.sh

--- Mapping/07_ASE_snps_filter_com.sh
+++ Mapping/07_ASE_snps_filter_com.sh
@@ -40,19 +40,3 @@ while read INPUT; do
 done < <(cut -f1,2 "${list}" | grep -v 'skip' | sort -u)
 
 
-module load STAR/2.7.10b-GCC-11.3.0
-cd /scratch/yz77862/mRNA
-
-for GENOME in KD4315-2 KD4315-4 KD4315-5 KD4315-8 yz306-2 yz306-3 yz306-5 yz306-6; do
-read1=/scratch/yz77862/mRNA/${GENOME}_R1_001_val_1.fq.gz
-read2=/scratch/yz77862/mRNA/${GENOME}_R2_001_val_2.fq.gz
-
-STAR --genomeDir /scratch/yz77862/ABS_Pacbio_index \
---runThreadN 6 \
---readFilesIn ${read1} ${read2} \
---readFilesCommand zcat \
---outSAMtype BAM SortedByCoordinate \
---outSAMunmapped Within \
---outSAMattributes Standard \
---outFileNamePrefix ${GENOME}
-done
