f7d8855d59aa0ae2babaf9e15f4de5b63223e5d8
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-05-01 15:03:59 -0400

    Update 06_SNPs_filter.sh

--- Mapping/06_SNPs_filter.sh
+++ Mapping/06_SNPs_filter.sh
@@ -1,40 +1,14 @@
-list=/scratch/yz77862/Allim/B73v5_Ki11/list
+#!/bin/bash
+#SBATCH --job-name=${INPUT}_mapping
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=200G
+#SBATCH --time=010:00:00
+#SBATCH --output=q20_com_VCF.out
+#SBATCH --error=q20_com_VCF_bam.err
 
-while read INPUT; do
+ml SAMtools
+cd /scratch/yz77862/Allim/gene_guide/output_default
 
-    OUT=/scratch/yz77862/Allim/gene_guide/shell/${INPUT}_VCF.sh
-
-    echo '#!/bin/bash'  > "${OUT}"
-    echo "#SBATCH --job-name=${INPUT}_mapping"   >> "${OUT}"
-    echo "#SBATCH --partition=batch"   >> "${OUT}"
-    echo "#SBATCH --nodes=1"   >> "${OUT}"
-    echo "#SBATCH --ntasks=1"   >> "${OUT}"
-    echo "#SBATCH --cpus-per-task=18"   >> "${OUT}"
-    echo "#SBATCH --mem=200G"   >> "${OUT}"
-    echo "#SBATCH --time=02:00:00"   >> "${OUT}"
-    echo "#SBATCH --output=${INPUT}_com_VCF.out"   >> "${OUT}"
-    echo "#SBATCH --error=${INPUT}_com_VCF_bam.err"   >> "${OUT}"
-    echo ""  >> "${OUT}"
-
-    echo "ml BCFtools/1.6-foss-2022a" >> "${OUT}"
-    echo "ml GATK/4.2.0.0-GCCcore-10.2.0" >> "${OUT}"  # Use 'ml' not 'module spider' for loading
-    echo "BAM=/scratch/yz77862/Allim/gene_guide/round2/${INPUT}_round-2Aligned.sortedByCoord.out.bam"  >> "${OUT}"
-    echo "ref=/scratch/yz77862/Allim/reference/B73v5_Ki11/B73v5_Ki11_1.0.fa"  >> "${OUT}"
-    echo "cd /scratch/yz77862/Allim/gene_guide/VCF" >> "${OUT}"
-
-    echo "bcftools mpileup -Ou -f \$ref \$BAM | bcftools call -mv -Oz -o ${INPUT}_raw.vcf.gz"  >> "${OUT}"
-    echo "gatk IndexFeatureFile -I ${INPUT}_raw.vcf.gz" >> "${OUT}"
-
-    echo "gatk VariantFiltration \\" >> "${OUT}"
-    echo " -R \$ref \\" >> "${OUT}" 
-    echo " -V ${INPUT}_raw.vcf.gz \\" >> "${OUT}"
-    echo " --filter-expression \"QD < 2.0\" --filter-name \"QD2\" \\" >> "${OUT}" 
-    echo " --filter-expression \"QUAL < 60.0\" --filter-name \"QUAL60\" \\" >> "${OUT}"
-    echo " --filter-expression \"SOR > 3.0\" --filter-name \"SOR3\" \\" >> "${OUT}"
-    echo " --filter-expression \"FS > 60.0\" --filter-name \"FS60\" \\" >> "${OUT}"
-    echo " --filter-expression \"MQ < 40.0\" --filter-name \"MQ40\" \\" >> "${OUT}"
-    echo " --filter-expression \"MQRankSum < -12.5\" --filter-name \"MQRS\" \\" >> "${OUT}"
-    echo " --filter-expression \"ReadPosRankSum < -8.0\" --filter-name \"RPRS\" \\" >> "${OUT}"
-    echo " -O ${INPUT}_filtered_snps.vcf" >> "${OUT}"
-
-done < <(cut -f1,2 "${list}" | grep -v 'skip' | sort -u)
