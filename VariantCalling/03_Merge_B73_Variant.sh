4f65f8d60750330e31f774e2c9584129def58c8a
Yibing <48507231+YibingZeng@users.noreply.github.com>
2025-06-30 17:59:28 -0400

    Update 03_Merge_B73_Variant.sh

--- VariantCalling/03_Merge_B73_Variant.sh
+++ VariantCalling/03_Merge_B73_Variant.sh
@@ -7,14 +7,12 @@ for vcf in *.vcf; do
   ## For the VCF file of B73 and KI11 F1 hybrid, it contains both B73 snps and Ki11 snps. To acquire Ki11 snps for reads filtering, that's how I did it:
   ## 1. I aligned B73 inbred to B73 reference genome and call the varaints.
   ## 2. I remove the varaints of B73 when mapped to B73
-  bcftools isec -C   ERR2889294_B73_raw.vcf.gz ERR2889272_B73_raw.vcf.gz -w1 -o ERR2889294-ERR2889272_filtered.vcf
+  bcftools isec -C   ERR2889294_B73_raw.vcf.gz ERR2889275_B73_raw.vcf.gz -w1 -o ERR2889294-ERR2889272_filtered.vcf
 
-  ##In F1 bam file, filter any reads specific to ERR2889294-ERR2889272_filtered.vcf
+  ## For the VCF file of B73 and KI11 F1 hybrid, it contains both B73 snps and Ki11 snps. To acquire Ki11 snps for reads filtering, that's how I did it:
+  ## 1. I aligned Ki11 inbred to Ki11 reference genome and call the varaints.
+  ## 2. I remove the varaints of Ki11 when mapped to Ki11
+ 
+  bcftools isec -C   ERR2889293_Ki11_raw.vcf.gz ERR2889288_Ki11_raw.vcf.gz -w1 -o ERR2889293-ERR2889288_filtered.vcf
 
-  bcftools query -f '%CHROM\t%POS\t%REF\n'  ERR2889294-ERR2889272_filtered.vcf  > ERR2889294-ERR2889272_filtered_sites.txt 
 
-
-ml bam-readcount
-cd /scratch/yz77862/Allim/gene_guide/B73/output_default
-ref=/scratch/yz77862/Allim/reference/B73/Zm-B73-REFERENCE-NAM-5.0.fa
-bam-readcount -f ${ref} -l ERR2889294-ERR2889272_filtered_sites.txt ERR2889294Aligned.sortedByCoord.out.bam > ERR2889294-ERR2889272_filtered_sites_readcounts.txt
